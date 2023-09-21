502da71e2157456dbeecff6a9e40b73eecf37eae
eden.kim <eden.kim@goorm.io>
2023-09-21 16:42:21 +0900

    style: config 파일의 코드 스타일 fix

--- krampoline/vite.config.js
+++ krampoline/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const env = loadEnv( process.cwd(), '')
-const staticServerUri = env.VITE_APP_PATH || "";  ;
+const env = loadEnv(process.cwd(), '')
+const staticServerUri = env.VITE_APP_PATH || "";
 
 export default defineConfig({
   base: staticServerUri,
   server: {
     port: 3000,
   },
-  plugins: [react(),],
+  plugins: [react()],
 });
